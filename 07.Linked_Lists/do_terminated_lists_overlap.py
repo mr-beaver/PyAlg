fec9da854431b6b29ddaeb7629b52a359db93359
patrichu <beavercoding@gmail.com>
2018-11-20 08:42:48 -0800

    delete node from list; list overlapping with no cycle;

--- 07.Linked_Lists/do_terminated_lists_overlap.py
+++ 07.Linked_Lists/do_terminated_lists_overlap.py
@@ -6,8 +6,31 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def overlapping_no_cycle_lists(l0, l1):
-    # TODO - you fill in here.
-    return None
+    # function returns the length of a list
+    def length(l):
+        count = 0
+        p = l
+        while p:
+            count += 1
+            p = p.next
+        return count
+
+    # get length of both list
+    len0, len1 = length(l0), length(l1)
+
+    # make sure l0 is always the longer list
+    if len1 > len0:
+        l0, l1 = l1, l0
+
+    # advance the pointer of longer list by difference of length
+    for _ in range(abs(len0 - len1)):
+        l0 = l0.next
+
+    # advance both pointers to check if there is overlap
+    while l0 and l1 and l0 is not l1:
+        l0, l1 = l0.next, l1.next
+
+    return l0
 
 
 @enable_executor_hook
